2df4071bb09aab343ae8a9f0b520892a4d069156
Kei Funagayama <kei.topaz@gmail.com>
2011-09-23 14:34:08 +0900

    flot sample finish

--- akiyoshi/static/js/akiyoshi.test.js
+++ akiyoshi/static/js/akiyoshi.test.js
@@ -7,31 +7,31 @@ $.akiyoshi.addHandler("test", new function() {
         /***** データ情報 *****/
         var datasets = {
             "usa": {
-                label: "USA",
+                label: "USA = 0",
                 data: [[1988, 483994], [1989, 479060], [1990, 457648], [1991, 401949], [1992, 424705], [1993, 402375], [1994, 377867], [1995, 357382], [1996, 337946], [1997, 336185], [1998, 328611], [1999, 329421], [2000, 342172], [2001, 344932], [2002, 387303], [2003, 440813], [2004, 480451], [2005, 504638], [2006, 528692]]
             },
             "russia": {
-                label: "Russia",
+                label: "Russia = 0",
                 data: [[1988, 218000], [1989, 203000], [1990, 171000], [1992, 42500], [1993, 37600], [1994, 36600], [1995, 21700], [1996, 19200], [1997, 21300], [1998, 13600], [1999, 14000], [2000, 19100], [2001, 21300], [2002, 23600], [2003, 25100], [2004, 26100], [2005, 31100], [2006, 34700]]
             },
             "uk": {
-                label: "UK",
+                label: "UK = 0",
                 data: [[1988, 62982], [1989, 62027], [1990, 60696], [1991, 62348], [1992, 58560], [1993, 56393], [1994, 54579], [1995, 50818], [1996, 50554], [1997, 48276], [1998, 47691], [1999, 47529], [2000, 47778], [2001, 48760], [2002, 50949], [2003, 57452], [2004, 60234], [2005, 60076], [2006, 59213]]
             },
             "germany": {
-                label: "Germany",
+                label: "Germany = 0",
                 data: [[1988, 55627], [1989, 55475], [1990, 58464], [1991, 55134], [1992, 52436], [1993, 47139], [1994, 43962], [1995, 43238], [1996, 42395], [1997, 40854], [1998, 40993], [1999, 41822], [2000, 41147], [2001, 40474], [2002, 40604], [2003, 40044], [2004, 38816], [2005, 38060], [2006, 36984]]
             },
             "denmark": {
-                label: "Denmark",
+                label: "Denmark = 0",
                 data: [[1988, 3813], [1989, 3719], [1990, 3722], [1991, 3789], [1992, 3720], [1993, 3730], [1994, 3636], [1995, 3598], [1996, 3610], [1997, 3655], [1998, 3695], [1999, 3673], [2000, 3553], [2001, 3774], [2002, 3728], [2003, 3618], [2004, 3638], [2005, 3467], [2006, 3770]]
             },
             "sweden": {
-                label: "Sweden",
+                label: "Sweden = 0",
                 data: [[1988, 6402], [1989, 6474], [1990, 6605], [1991, 6209], [1992, 6035], [1993, 6020], [1994, 6000], [1995, 6018], [1996, 3958], [1997, 5780], [1998, 5954], [1999, 6178], [2000, 6411], [2001, 5993], [2002, 5833], [2003, 5791], [2004, 5450], [2005, 5521], [2006, 5271]]
             },
             "norway": {
-                label: "Norway",
+                label: "Norway = 0",
                 data: [[1988, 4382], [1989, 4498], [1990, 4535], [1991, 4398], [1992, 4766], [1993, 4441], [1994, 4670], [1995, 4217], [1996, 4275], [1997, 4203], [1998, 4482], [1999, 4506], [2000, 4358], [2001, 4385], [2002, 5269], [2003, 5066], [2004, 5194], [2005, 4887], [2006, 4891]]
             }
         };
@@ -43,23 +43,29 @@ $.akiyoshi.addHandler("test", new function() {
                 points: {show: true} // 値のところにポイント（丸）が置かれる
             },
             legend: {
-                noColumns: 2
+                noColumns: 1,
+                show: true,
+                container: $("#overviewLegend")
             },
             xaxis: {
                 tickDecimals: 0
             },
             yaxis: {
-                min: 0
+                min: 0,
+                ticks: 10
             },
             selection: {
-                mode: "x"
+                mode: "xy"
             },
             grid: {
                 hoverable: true,
-                clickable: true
+                clickable: true,
+                autoHighlight: false
+            },
+            crosshair: {
+                mode: "x"
             }
         };
-
 
         /***** 関数 *****/        
         function showTooltip(x, y, contents) {
@@ -74,7 +80,37 @@ $.akiyoshi.addHandler("test", new function() {
                 opacity: 0.80
             }).appendTo("body").fadeIn(200);
         };
+        
+        function updateLegend() {
+            updateLegendTimeout = null;
+            var pos = latestPosition;
+            var axes = plot.getAxes();
+            if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+                pos.y < axes.yaxis.min || pos.y > axes.yaxis.max)
+                return;
 
+            var i, j, dataset = plot.getData();
+            for (i = 0; i < dataset.length; ++i) {
+                var series = dataset[i];
+                // find the nearest points, x-wise
+                for (j = 0; j < series.data.length; ++j)
+                    if (series.data[j][0] > pos.x)
+                        break;
+                // now interpolate
+                var y, p1 = series.data[j - 1], p2 = series.data[j];
+                if (p1 == null)
+                    y = p2[1];
+                else if (p2 == null)
+                    y = p1[1];
+                else
+                    y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+
+                //legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
+                legends.find(".legendLabel").eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
+                
+            }
+        };
+        
         // チェック項目をグラフ描画情報に設定
         function plotAccordingToChoices() {
             var data = [];
@@ -86,12 +122,53 @@ $.akiyoshi.addHandler("test", new function() {
             
             if (data.length > 0) {
                 plot = $.plot($("#placeholder"), data, $.extend(true, {}, options));
+
+                //legends = $("#placeholder .legendLabel");
+                legends = $("#miniature #overviewLegend");
+                legends.each(function () {
+                    $(this).css('width', $(this).width());
+                });
+
+
+                // miniture setup overview
+                var ovdata = [];
+                for (var key in datasets) {
+                    ovdata.push(datasets[key]);
+                }
+                overview = $.plot($("#overview"), ovdata, {
+                    legend: {
+                        show: false,
+                    },
+                    series: {
+                        lines: {
+                            show: true,
+                            lineWidth: 1
+                        },
+                        shadowSize: 0
+                    },
+                    xaxis: {
+                        ticks: 4
+                    },
+                    yaxis: {
+                        ticks: 3,
+                    },
+                    grid: {
+                        color: "#999"
+                    },
+                    selection: {
+                        mode: "xy"
+                    }
+                });
             }
         }
 
         /***** メンバ変数 *****/
         var plot = null;
         var previousPoint = null;
+        var updateLegendTimeout = null;
+        var latestPosition = null;
+        var legends = null;
+        var overview = null;
 
         /***** メイン処理 *****/
         var i = 0;
@@ -130,9 +207,10 @@ $.akiyoshi.addHandler("test", new function() {
 
         // 項目checkboxをクリックするとグラフを再描画
         choiceContainer.find("input").click(plotAccordingToChoices);
-        plotAccordingToChoices();
         
         $("#placeholder").bind("plothover", function (event, pos, item) {
+
+            // Show mouse position
             if ($("#enablePosition:checked").length > 0) {
                 var str = "(" + pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + ")";
                 $("#hoverdata").attr("value", str);
@@ -156,16 +234,32 @@ $.akiyoshi.addHandler("test", new function() {
                     previousPoint = null;
                 }
             }
+
+            latestPosition = pos;
+            if (!updateLegendTimeout)
+                updateLegendTimeout = setTimeout(updateLegend, 50);
         });
 
-        /**
-        $("#placeholder").bind("plotclick", function (event, pos, item) {
-            debugger;
-            if (item) {
-                $("#clickdata").text(" - click point " + item.dataIndex + " in " + item.series.label);
-                plot.highlight(item.series, item.datapoint);
-            }
+    
+        $("#placeholder").bind("plotselected", function (event, ranges) {
+            if (ranges.xaxis.to - ranges.xaxis.from < 0.00001)
+                ranges.xaxis.to = ranges.xaxis.from + 0.00001;
+            if (ranges.yaxis.to - ranges.yaxis.from < 0.00001)
+                ranges.yaxis.to = ranges.yaxis.from + 0.00001;
+                
+            plot = $.plot($("#placeholder"), plot.getData(ranges.xaxis.from, ranges.xaxis.to),
+                          $.extend(true, {}, options, {
+                              xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
+                              yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
+                          }));
+        
+            overview.setSelection(ranges, true);
         });
-        **/
+        $("#overview").bind("plotselected", function (event, ranges) {
+            plot.setSelection(ranges);
+        });
+
+        // 描画する
+        plotAccordingToChoices();
     };
 });
